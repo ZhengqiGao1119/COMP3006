0d054db24ab913c45550f2d61f7f6c82624e8bad
ZhengqiGao1119 <148078584+ZhengqiGao1119@users.noreply.github.com>
2024-01-08 20:50:18 +0800

    Delete vue-admin

--- vue-admin/src/api/home.js
+++
@@ -1,8 +0,0 @@
-import service from "../utils/request"
-
-export function findHome () {
-  return service({
-    url: '/home',
-    method: 'get'
-  })
-}
--- vue-admin/src/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    API_URL: "http://localhost:3002/api",
-};
-
-export default config
