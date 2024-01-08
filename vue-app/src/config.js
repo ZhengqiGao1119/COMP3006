3f841d995802a7eafce1509e88597517654ab9a3
ZhengqiGao1119 <148078584+ZhengqiGao1119@users.noreply.github.com>
2024-01-08 20:50:32 +0800

    Delete vue-app

--- vue-app/src/config.js
+++
@@ -1,8 +0,0 @@
-// Customized configuration file.
-const config = {
-    // Backend API address.:dev / online
-    API_URL: "http://localhost:3002/api",
-    // API_URL: "http://47.107.29.171:8888/api/cp",
-};
-
-export default config
